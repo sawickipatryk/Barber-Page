52f69b1e5528c22ab77dc0a1058ff8f6d17381e6
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-11-30 13:47:27 +0100

    gallery done

---
+++ assets/js/gallery.js
@@ -0,0 +1,22 @@
+const navLinks = document.querySelector('.nav__links')
+const toggleMenu = document.querySelector('.toggle__menu')
+
+toggleMenu.addEventListener('click', () => {
+    if (!navLinks.classList.contains('active')) {
+        navLinks.classList.add('active')
+        return
+    }
+    navLinks.classList.remove('active')
+})
+
+const navItemParent = document.querySelector('.nav__item--parent')
+const navSublist = document.querySelector('.nav__sublist')
+console.log(navItemParent)
+
+navItemParent.addEventListener('click', () => {
+    if (!navSublist.classList.contains('active')) {
+        navSublist.classList.add('active')
+        return
+    }
+    navSublist.classList.remove('active')
+})
