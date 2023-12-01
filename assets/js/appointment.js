3695b05c76b1401ac16d659547383861bd5fd775
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-12-01 10:54:49 +0100

    added IIFE to scripts

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -1,23 +1,24 @@
-const navLinks = document.querySelector('.nav__links')
-const toggleMenu = document.querySelector('.toggle__menu')
+const app = (function () {
+    const navLinks = document.querySelector('.nav__links')
+    const toggleMenu = document.querySelector('.toggle__menu')
 
-toggleMenu.addEventListener('click', () => {
-    if (!navLinks.classList.contains('active')) {
-        navLinks.classList.add('active')
-        return
-    }
-    navLinks.classList.remove('active')
-})
+    toggleMenu.addEventListener('click', () => {
+        if (!navLinks.classList.contains('active')) {
+            navLinks.classList.add('active')
+            return
+        }
+        navLinks.classList.remove('active')
+    })
 
-const navItemParent = document.querySelector('.nav__item--parent')
-const navSublist = document.querySelector('.nav__sublist')
-console.log(navItemParent)
+    const navItemParent = document.querySelector('.nav__item--parent')
+    const navSublist = document.querySelector('.nav__sublist')
 
-navItemParent.addEventListener('click', () => {
-    if (window.innerWidth > 1200) return
-    if (!navSublist.classList.contains('active')) {
-        navSublist.classList.add('active')
-        return
-    }
-    navSublist.classList.remove('active')
-})
+    navItemParent.addEventListener('click', () => {
+        if (window.innerWidth > 1200) return
+        if (!navSublist.classList.contains('active')) {
+            navSublist.classList.add('active')
+            return
+        }
+        navSublist.classList.remove('active')
+    })
+})()
